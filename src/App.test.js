9801a833f08a98f6cdba21f074b74adc6b77ac5c
Pob <590650@gmail.com>
2019-10-12 13:32:47 +0700

    added @types/jest and manually add jest-dom type into the project, then wrote the first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+test('App is rendered', () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+  expect(getByText(/contact/i)).toBeInTheDocument()
+})
